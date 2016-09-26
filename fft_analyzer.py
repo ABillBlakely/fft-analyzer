3710d560bf23db15834c705d86b7d7658adbf9be
William Blakely <ABillBlakely@gmail.com>
2016-09-25 20:12:51 -0700

    Add basic sine wave synthesis for test tones.

--- fft_analyzer.py
+++ fft_analyzer.py
@@ -3,6 +3,7 @@ from collections import deque
 import matplotlib.pyplot as plt
 import sounddevice as sd
 from matplotlib.animation import FuncAnimation
+import numpy as np
 from numpy import abs, fft, log10, seterr
 
 # Some aliases to make things readable.
@@ -11,8 +12,9 @@ RIGHT = 1
 
 # The Queue where the input data will be stored.
 indataQ = deque(maxlen=10)
-
+out_sig = None
 seterr(divide='ignore')
+cumulated_status = sd.CallbackFlags()
 
 
 class AudioStream():
@@ -22,8 +24,19 @@ class AudioStream():
 
         def audio_callback(indata, outdata, frames, time, status):
                 '''Called by audio stream for each new buffer.'''
+                global cumulated_status
                 indataQ.append(indata[::, LEFT])
-                outdata.fill(0)
+                if out_sig.any():
+                    outdata[:, LEFT] = out_sig
+                else:
+                    outdata.fill(0)
+                cumulated_status |= status
+
+        # Checks will throw errors for invalid settings.
+        sd.check_input_settings()
+        sd.check_output_settings()
+
+        self.args = args
 
         # Create the stream
         self.audio_stream = sd.Stream(callback=audio_callback,
@@ -33,15 +46,26 @@ class AudioStream():
                                       clip_off=True,
                                       dither_off=args.dither)
 
-    def output_signal(freq, Fs, buf_size,):
-        '''Create the signal for output'''
-        None
+    def create_output_signal(self, freq=1000, level=0.98):
+        '''Return numpy array of the output signal.'''
+        global out_sig
+        freq_array = fft.rfftfreq(n=self.args.buff_size,
+                                  d=(1 / self.args.sample_rate))
+        mag_array = np.zeros_like(freq_array)
+        closest_freq = np.searchsorted(freq_array, freq)
+        mag_array[closest_freq] = level * self.args.buff_size / 2
+        output = np.fft.irfft(a=mag_array, n=self.args.buff_size)
+        out_sig = output.copy()
+        return(output)
 
     def start_stream(self):
+        self.create_output_signal()
         self.audio_stream.start()
 
     def stop_stream(self):
+        global cumulated_status
         self.audio_stream.stop()
+        print(str(cumulated_status))
 
     def reload(self):
         self.audio_stream.close()
@@ -59,7 +83,8 @@ class FFTDisplay():
     def __init__(self, args):
         self.args = args
         # Determine the frequencies the dfft calculates at.
-        self.freq = fft.rfftfreq(args.fft_size, 1 / args.sample_rate)
+        self.freq = fft.rfftfreq(n=args.fft_size,
+                                 d=(1 / args.sample_rate))
         # Setup the display:
         self.fig = plt.figure()
         self.ax = plt.axes(xlim=args.xlims, ylim=args.ylims)
@@ -77,7 +102,7 @@ class FFTDisplay():
         try:
             a_in = indataQ.popleft()
             mag_in_dB = 20 * log10(abs(
-                        fft.rfft(a_in, n=self.args.fft_size)
+                        fft.rfft(a=a_in, n=self.args.fft_size)
                         * 2 / self.args.buff_size))
             self.line.set_data(self.freq, mag_in_dB)
         except IndexError:
